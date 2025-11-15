dfd3e00b645ad644f28389a74ebd90ea7d28f68c
ifedips20s <ifedips20s@gmail.com>
2025-11-16 00:26:34 +0100

    Initial commit: project setup with models, serializers, and seed command

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ifedips/desktop/alx_travel_app/venv
+set -gx VIRTUAL_ENV /home/ifedips/desktop/alx_travel_app_0x00/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
